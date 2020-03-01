118347c5c437fadc236c36c4162d0d0c320a5a1a
zhucj <zhucj@servyou.com.cn>
2020-03-01 20:39:56 +0800

    init

--- src/com/janita/plugin/test/HelloTestAction.java
+++ src/com/janita/plugin/test/HelloTestAction.java
@@ -1,4 +1,4 @@
-package com.janita.plugin;
+package com.janita.plugin.test;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
--- src/com/janita/plugin/translate/TestTranslateAction.java
+++ src/com/janita/plugin/translate/TestTranslateAction.java
@@ -1,15 +1,25 @@
 package com.janita.plugin.translate;
 
+import com.google.gson.Gson;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.openapi.ui.popup.BalloonBuilder;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.janita.plugin.domain.TranslationResult;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
 import okhttp3.Call;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
 
 /**
  * @author zhucj
@@ -18,6 +28,7 @@ public class TestTranslateAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
+        Project project = e.getData(PlatformDataKeys.PROJECT);
         //获取用户选择的内容
         //获取当前用户所在的编辑器对象
         Editor editor = e.getData(PlatformDataKeys.EDITOR);
@@ -31,9 +42,8 @@ public class TestTranslateAction extends AnAction {
         if (StringUtils.isEmpty(selectedText)) {
             return;
         }
-        System.out.println("选中的文本：" + selectedText + "，开始调翻译接口");
-
         //调翻译接口
+        Gson gson = new Gson();
         OkHttpUtils.post()
                 .url("http://fanyi.youdao.com/openapi.do")
                 .addParams("keyfrom", "HMTranslate")
@@ -45,16 +55,31 @@ public class TestTranslateAction extends AnAction {
                 .build().execute(new StringCallback() {
             @Override
             public void onError(Call call, Exception e, int i) {
-                System.out.println("请求翻译失败");
+                ApplicationManager.getApplication().invokeLater(() -> Messages.showErrorDialog(project, "请求翻译失败", "标题"));
             }
 
             @Override
             public void onResponse(String s, int i) {
-                System.out.println("请求翻译成功：" + s);
+                TranslationResult translationResult = gson.fromJson(s, TranslationResult.class);
+                ApplicationManager.getApplication().invokeLater(() -> showPop(translationResult, editor));
             }
         });
-
-        //展示
-
     }
+
+    /**
+     * 显示气泡
+     */
+    private void showPop(TranslationResult translationResult, Editor editor) {
+        //pop弹窗
+        JBPopupFactory factory = JBPopupFactory.getInstance();
+        BalloonBuilder builder = factory.createHtmlTextBalloonBuilder(translationResult.toString(),
+                null, new Color(188, 238, 188),
+                new Color(73, 120, 73), null);
+        //        无操作5s隐藏,
+        builder.createBalloon();
+        builder.setFadeoutTime(5000L)
+                .createBalloon()
+                .show(factory.guessBestPopupLocation(editor), Balloon.Position.below);
+    }
+
 }
