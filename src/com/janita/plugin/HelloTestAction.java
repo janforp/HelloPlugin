70d6c9f269fd4a9dabcddcad71f99d8d942bd895
zhucj <zhucj@servyou.com.cn>
2020-03-01 16:49:45 +0800

    init

---
+++ src/com/janita/plugin/HelloTestAction.java
@@ -0,0 +1,15 @@
+package com.janita.plugin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author zhucj
+ */
+public class HelloTestAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
